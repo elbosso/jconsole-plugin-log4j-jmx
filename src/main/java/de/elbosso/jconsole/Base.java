12c5e9b835817ff417a50cd0890422b5c3958f97
EL BOSSO <jkey@arcor.de>
2018-05-15 09:34:53 +0200

    re #22 selected items

--- src/main/java/de/elbosso/jconsole/Base.java
+++ src/main/java/de/elbosso/jconsole/Base.java
@@ -76,26 +76,8 @@ abstract class Base extends javax.swing.JPanel implements javax.management.Notif
 				{
 					gpid=de.netsysit.ui.dialog.GeneralPurposeInfoDialog.create(Base.this,"Preferences");
 					preferencesPanel=new javax.swing.JPanel(new java.awt.BorderLayout());
-					javax.swing.JPanel levelContainer=new javax.swing.JPanel(new java.awt.BorderLayout());
-					javax.swing.JTable t=new javax.swing.JTable(levelModel);
-					levelContainer.add(new javax.swing.JScrollPane(t));
-					javax.swing.JToolBar tb=new javax.swing.JToolBar();
-					tb.setFloatable(false);
-					tb.add(levelModel.getSelectAllAction());
-					tb.add(levelModel.getSelectNoneAction());
-					tb.add(levelModel.getToggleSelectionAction());
-					levelContainer.add(tb,BorderLayout.NORTH);
-					preferencesPanel.add(levelContainer,BorderLayout.NORTH);
-					javax.swing.JPanel loggerContainer=new javax.swing.JPanel(new java.awt.BorderLayout());
-					t=new javax.swing.JTable(loggerModel);
-					loggerContainer.add(new javax.swing.JScrollPane(t));
-					tb=new javax.swing.JToolBar();
-					tb.setFloatable(false);
-					tb.add(loggerModel.getSelectAllAction());
-					tb.add(loggerModel.getSelectNoneAction());
-					tb.add(loggerModel.getToggleSelectionAction());
-					loggerContainer.add(tb,BorderLayout.NORTH);
-					preferencesPanel.add(loggerContainer);
+					preferencesPanel.add(new PreferencesPart(levelModel),BorderLayout.NORTH);
+					preferencesPanel.add(new PreferencesPart(loggerModel));
 				}
 				gpid.showDialog(preferencesPanel);
 			}
---
+++ src/main/java/de/elbosso/jconsole/PreferencesPart.java
@@ -0,0 +1,85 @@
+package de.elbosso.jconsole;
+
+import javax.swing.event.ListSelectionEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class PreferencesPart extends javax.swing.JPanel implements javax.swing.event.ListSelectionListener
+{
+	private javax.swing.Action selectAllAction;
+	private javax.swing.Action selectNoneAction;
+	private javax.swing.Action toggleSelectionAction;
+	private javax.swing.JTable t;
+
+	PreferencesPart(StringBooleanModel model)
+	{
+		super(new java.awt.BorderLayout());
+		createActions();
+		t=new javax.swing.JTable(model);
+		add(new javax.swing.JScrollPane(t));
+		javax.swing.JToolBar tb=new javax.swing.JToolBar();
+		tb.setFloatable(false);
+		tb.add(model.getSelectAllAction());
+		tb.add(model.getSelectNoneAction());
+		tb.add(model.getToggleSelectionAction());
+		tb.addSeparator();
+		tb.add(selectAllAction);
+		tb.add(selectNoneAction);
+		tb.add(toggleSelectionAction);
+		add(tb,BorderLayout.NORTH);
+		t.getSelectionModel().addListSelectionListener(this);
+	}
+	private void createActions()
+	{
+		selectAllAction=new javax.swing.AbstractAction("select all",new javax.swing.ImageIcon(de.netsysit.util.ResourceLoader.getImgResource("de/elbosso/ressources/gfx/eb/select all_48.png")))
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				for(int i=0;i<t.getRowCount();++i)
+				{
+					if(t.getSelectionModel().isSelectedIndex(i))
+						t.setValueAt(Boolean.TRUE,i,1);
+				}
+			}
+		};
+		selectAllAction.setEnabled(false);
+		selectNoneAction=new javax.swing.AbstractAction("select none",new javax.swing.ImageIcon(de.netsysit.util.ResourceLoader.getImgResource("de/elbosso/ressources/gfx/eb/deselect all_48.png")))
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				for(int i=0;i<t.getRowCount();++i)
+				{
+					if(t.getSelectionModel().isSelectedIndex(i))
+						t.setValueAt(Boolean.FALSE,i,1);
+				}
+			}
+		};
+		selectNoneAction.setEnabled(false);
+		toggleSelectionAction=new javax.swing.AbstractAction("toggle selection",new javax.swing.ImageIcon(de.netsysit.util.ResourceLoader.getImgResource("de/elbosso/ressources/gfx/eb/toggle_selection_48.png")))
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				for(int i=0;i<t.getRowCount();++i)
+				{
+					if(t.getSelectionModel().isSelectedIndex(i))
+						t.setValueAt(((java.lang.Boolean)t.getValueAt(i,1)).booleanValue()?Boolean.FALSE:Boolean.TRUE,i,1);
+				}
+			}
+		};
+		toggleSelectionAction.setEnabled(false);
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e)
+	{
+		if(e.getValueIsAdjusting()==false)
+		{
+			selectAllAction.setEnabled(t.getSelectionModel().isSelectionEmpty()==false);
+			selectNoneAction.setEnabled(t.getSelectionModel().isSelectionEmpty()==false);
+			toggleSelectionAction.setEnabled(t.getSelectionModel().isSelectionEmpty()==false);
+		}
+	}
+}
